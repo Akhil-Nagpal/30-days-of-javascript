73bcb57b08a53aedf1cd9e3b639ab387a3ade701
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-07-19 17:15:29 +0530

    Refactored Code

--- Day 7/day7.js
+++ Day 7/day7.js
@@ -1,14 +1,17 @@
+// *-*-*-*-* Task 1 *-*-*-*-*
 // Define a book object with title, author, and year properties
 let book = {
   title: "Twilight Saga",
   author: "Stephenie Meyer",
   year: "2005-2016",
 
+  // *-*-*-*-* Task 3 *-*-*-*-*
   // Method to get the title and author of the book
   getBookTitleAuthor: function () {
     return `${this.title} written by ${this.author}`;
   },
 
+  // *-*-*-*-* Task 4 *-*-*-*-*
   // Method to update the year and return the new value
   getYear: function (year) {
     this.year = year; // Update the year property
@@ -16,14 +19,25 @@ let book = {
   },
 };
 
+// ----- Task 1 -> Output -----
 // Log the entire book object
 console.log(book);
 
+// *-*-*-*-* Task 2 *-*-*-*-*
 // Log a formatted string using properties of the book object
 console.log(
   `New York Bestseller ${book.title} written by ${book.author}`
 );
 
+// ----- Task 3 -> Output -----
+// Log the result of getBookTitleAuthor method of the book object
+console.log(book.getBookTitleAuthor());
+
+// ----- Task 4 -> Output -----
+// Log the result of getYear method with a new year value
+console.log(book.getYear(2005));
+
+// *-*-*-*-* Task 5 *-*-*-*-*
 // Define a library object with a name and an array of books
 let library = {
   name: "Admont Abbey Library",
@@ -35,6 +49,7 @@ let library = {
     { title: "The God Delusion", year: "2006" },
   ],
 
+  // *-*-*-*-* Task 7 *-*-*-*-*
   // Method to get a list of book details
   getBooksDetails: function () {
     return this.books.map(
@@ -43,15 +58,7 @@ let library = {
   },
 };
 
-// Log the result of getBookTitleAuthor method of the book object
-console.log(book.getBookTitleAuthor());
-
-// Log the result of getYear method with a new year value
-console.log(book.getYear(2005));
-
-// Log the updated book object
-console.log(book);
-
+// ----- Task 5 -> Output -----
 // Log the entire library object
 console.log(library);
 
@@ -74,6 +81,7 @@ console.log(library);
 // // Call the accessLibrary function
 // accessLibrary();
 
+// *-*-*-*-* Task 6 *-*-*-*-*
 // Define another version of the accessLibrary function
 function accessLibrary() {
   const lib = library.name; // Access the library name
@@ -90,23 +98,27 @@ function accessLibrary() {
     `In ${lib} you can find plenty of books like ${book.join(", ")}`
   );
 }
-
+// ----- Task 6 -> Output -----
 // Call the accessLibrary function
 accessLibrary();
 
+// ----- Task 7 -> Output -----
 // Log the details of all books using the getBooksDetails method of the library object
 console.log(library.getBooksDetails());
 
+// *-*-*-*-* Task 8 *-*-*-*-*
 // Use for...in loop to iterate over the indices of the books array
 for (const index in library.books) {
   const book = library.books[index]; // Access the book object at the current index
 
+  // ----- Task 8 -> Output -----
   // Log the title and publication year of the book
   console.log(
     `Title: ${book.title}, Year(s) Published: ${book.year}`
   );
 }
 
+// *-*-*-*-* Task 9 *-*-*-*-*
 // Define a function to get and log key-value pairs of book properties
 function getKeyValuePair() {
   const keyValuePair = library.books.map((book) => {
@@ -119,10 +131,30 @@ function getKeyValuePair() {
   console.table(keyValuePair); // Log the key-value pairs in a table format
 }
 
+// ----- Task 9 -> Output -----
 // Call the getKeyValuePair function
 getKeyValuePair();
 
-// Define another version of the getKeyValuePair function
+// *-*-*-*-* Task 6 -> Another Way *-*-*-*-*
+// Define a function to access and log library details
+// function accessLibrary() {
+//   const lib = library.name; // Access the library name
+//   let book = []; // Initialize an empty array to store book titles
+
+//   // Use map to create an array of book titles
+//   const books = library.books.map((item) => {
+//     return item.title; // Return the title of each book
+//   });
+//   console.log(books); // Log the array of book titles
+
+//   // Log a formatted string with the library name and book titles
+//   console.log(`In ${lib} you can find plenty of books like ${books}`);
+// }
+
+// // Call the accessLibrary function
+// accessLibrary();
+
+// *-*-*-*-* Task 9 -> Another Way *-*-*-*-*
 // function getKeyValuePair() {
 //   const keyValuePairs = library.books.map((book) => {
 //     // Iterate through book properties using for...in loop
